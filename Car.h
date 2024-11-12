05d9aa7f6bcb84bd669f2ee21e9362ea0a807201
Reets123 <ibrahimalatig@gmail.com>
2024-11-12 23:54:45 +0000

    commit 1

---
+++ Car.h
@@ -0,0 +1,17 @@
+// Lab 32: Toll Booth
+// COMSC-210 - Ibrahim Alatig 
+// Car.h
+
+#ifndef CAR_H
+#define CAR_H
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class Car {
+private:
+    string make;
