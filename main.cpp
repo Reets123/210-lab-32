e27c710ebb82b6d459424d0da6e82b6ba4c52f9f
Reets123 <ibrahimalatig@gmail.com>
2024-11-12 10:19:09 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Lab 32: Toll Booth
+// COMSC-210 - Ibrahim Alatig 
+
+#include <iostream>
+#include <deque>
+#include <cstdlib>
+#include <ctime>
+#include "Car.h" // Include the Car class
+
+using namespace std;
+
+// Constants
+const int INITIAL_CAR_COUNT = 2;
+const int TOTAL_YEARS = 26; // 1999 to 2024
