45e037610fa327e3775ec9bde966b547f19b9a65
Reets123 <ibrahimalatig@gmail.com>
2024-11-13 10:14:03 +0000

    commit 9

--- Car.h
+++ Car.h
@@ -6,34 +6,35 @@
 #define CAR_H
 
 #include <iostream>
-#include <vector>
+#include <string>
 #include <cstdlib>
 #include <ctime>
 
 using namespace std;
 
 class Car {
+public:
+    Car();
+    void print() const;
+
 private:
-    string make;int year;
+    string make;
+    int year;
     int transponder;
 
-    static const vector<string> manufacturers;
-
-public:
-    Car() { srand(static_cast<unsigned int>(time(0))); // for randomness
-        make = manufacturers[rand() % manufacturers.size()];
-        year = rand() % 26 + 1999; // Random year between 1999 to 2024
-        transponder = (rand() % 9000) + 1000; // Random number between 1000 to 9999
-    }
-
-    void print() const {
-        cout << year << " " << make << " (" << transponder << ")" << endl;
-    }
-
+    static const string makes[];
 };
 
-const vector<string> Car::manufacturers = {"Toyota", "Honda", "Ford", "Chevrolet", "Volkswagen", 
-                                            "Nissan", "BMW", "Mercedes", "Tesla", "Lexus", 
-                                            "Skoda", "Volvo"};
+const string Car::makes[] = {"Volkswagen", "Skoda", "Volvo", "Lexus", "Toyota"};
 
-#endif
+Car::Car() {
+    make = makes[rand() % (sizeof(makes) / sizeof(makes[0]))];
+    year = rand() % 26 + 1999; // Random year between 1999-2024
+    transponder = rand() % 9000 + 1000; // Random transponder between 1000-9999
+}
+
+void Car::print() const {
+    cout << "[" << year << " " << make << " (" << transponder << ")]";
+}
+
+#endif 
