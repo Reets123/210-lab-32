013b2e5c8592f366a61f2183740e2dc9a6fe83fc
Reets123 <ibrahimalatig@gmail.com>
2024-11-13 05:44:38 +0000

    commit 4

--- main.cpp
+++ main.cpp
@@ -12,3 +12,21 @@ using namespace std;
 // Constants
 const int INITIAL_CAR_COUNT = 2;
 const int TOTAL_YEARS = 26; // 1999 to 2024
+
+int main() {
+    deque<Car> tollQueue;
+
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    for (int i = 0; i < INITIAL_CAR_COUNT; ++i) {
+        tollQueue.push_back(Car());
+    }
+
+    cout << "Initial queue:" << endl;
+    for (const Car &car : tolQueue) {
+        car.print();
+    }
+    cout << endl;
+
+    int cycle = 0;
+    while (!tollQueue.empty()) {
